68c4cd056de2ebf16700e54c6e8cfcfdaafe88b2
hyosunko <hyosunko@gmail.com>
2018-12-30 17:04:22 -0800

    Updates

---
+++ precache-manifest.8a933fce21875a13bab78671ae8f1d39.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "26ead58317a649cdf406",
+    "url": "/tic-tac-toe/static/js/runtime~main.26ead583.js"
+  },
+  {
+    "revision": "9792785a743e4bf240df",
+    "url": "/tic-tac-toe/static/js/main.9792785a.chunk.js"
+  },
+  {
+    "revision": "f739feb7d61c48a46bed",
+    "url": "/tic-tac-toe/static/js/1.f739feb7.chunk.js"
+  },
+  {
+    "revision": "9792785a743e4bf240df",
+    "url": "/tic-tac-toe/static/css/main.10971608.chunk.css"
+  },
+  {
+    "revision": "124a418bfc28cf7779142f729afe65b3",
+    "url": "/tic-tac-toe/index.html"
+  }
+];
